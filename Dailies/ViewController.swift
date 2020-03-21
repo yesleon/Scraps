9a2467fb8cc1d30cd78da0f5431a061d3bb0d5bb
Li-Heng Hsu <yesleon@me.com>
2020-03-21 18:11:08 +0800

    Initial Commit

---
+++ Dailies/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Dailies
+//
+//  Created by Li-Heng Hsu on 2020/3/21.
+//  Copyright © 2020 Li-Heng Hsu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
